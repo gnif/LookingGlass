4fd62a58bd8a0f8b0d05c67f433fe53f821725a3
Geoffrey McRae <geoff@hostfission.com>
2019-02-28 19:44:15 +1100

    [c-host] move dxgi capture into windows directory

--- c-host/capture/interfaces.h
+++ c-host/capture/interfaces.h
@@ -20,7 +20,7 @@ Place, Suite 330, Boston, MA 02111-1307 USA
 #include "interface.h"
 
 #if defined(USE_DXGI)
-extern struct CaptureInterface Capture_DXGI;
+struct CaptureInterface Capture_DXGI;
 #endif
 
 struct CaptureInterface * CaptureInterfaces[] =
--- c-host/windows/capture/dxgi.c
+++ c-host/windows/capture/dxgi.c
@@ -17,7 +17,7 @@ this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
-#include "interface.h"
+#include "capture/interface.h"
 #include "debug.h"
 #include "windows/windebug.h"
 
