893b23f3cda21b73129a2659db4bf3ed5c4f4fb4
Geoffrey McRae <geoff@hostfission.com>
2020-01-26 18:50:07 +1100

    [c-host] increase lgmp host process resolution

--- c-host/src/app.c
+++ c-host/src/app.c
@@ -94,7 +94,7 @@ static int lgmpThread(void * opaque)
       DEBUG_ERROR("lgmpHostProcess Failed: %s", lgmpStatusString(status));
       break;
     }
-    usleep(1000);
+    usleep(10);
   }
 
   app.running = false;
