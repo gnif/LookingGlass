6e62ea5364ad23b21560904d648a305afc6354c4
Geoffrey McRae <geoff@hostfission.com>
2020-01-28 05:10:38 +1100

    [common] fix building on mingw for linux

--- common/include/common/debug.h
+++ common/include/common/debug.h
@@ -17,6 +17,10 @@ this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
+#ifndef __STDC_FORMAT_MACROS
+#define __STDC_FORMAT_MACROS
+#endif
+
 #include <stdio.h>
 #include <inttypes.h>
 #include "time.h"
