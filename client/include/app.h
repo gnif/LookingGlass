076ed1180faa097a21f03d3f290a3917ce3142f6
Geoffrey McRae <geoff@hostfission.com>
2021-01-17 13:33:23 +1100

    [client] app: expose app_handleFocusEvent to displayservers

--- client/include/app.h
+++ client/include/app.h
@@ -37,6 +37,7 @@ void app_handleMouseNormal(double ex, double ey);
 void app_handleMouseBasic(void);
 void app_handleWindowEnter(void);
 void app_handleWindowLeave(void);
+void app_handleFocusEvent(bool focused);
 
 void app_clipboardRelease(void);
 void app_clipboardNotify(const LG_ClipboardData type, size_t size);
