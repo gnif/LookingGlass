6990d7f7e3394a77da28bfd9e24765410a3f19fd
Geoffrey McRae <geoff@hostfission.com>
2021-01-24 12:16:39 +1100

    [client] egl: commit missed files for the last changeset

---
+++ client/renderers/EGL/dynprocs.h
@@ -0,0 +1,32 @@
+/*
+Looking Glass - KVM FrameRelay (KVMFR) Client
+Copyright (C) 2017-2021 Geoffrey McRae <geoff@hostfission.com>
+https://looking-glass.hostfission.com
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+#include <SDL2/SDL_egl.h>
+#include <GL/gl.h>
+
+typedef void (*glEGLImageTargetTexture2DOES_t)(GLenum target, GLeglImageOES image);
+
+struct EGLDynProcs
+{
+  glEGLImageTargetTexture2DOES_t glEGLImageTargetTexture2DOES;
+};
+
+extern struct EGLDynProcs g_dynprocs;
+
+void egl_DynProcsInit(void);
