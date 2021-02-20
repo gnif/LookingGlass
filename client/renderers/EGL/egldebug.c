67003b4c28ee60542068c5d52a62fb5aa64f7220
Quantum <quantum2048@gmail.com>
2021-02-20 01:45:38 -0500

    [client] egl: remove dependency on SDL
    After this PR, EGL functions are now accessed through <EGL/egl.h>
    instead of through <SDL2/SDL_egl.h>, removing a pointless dependency
    on SDL.

--- client/renderers/EGL/egldebug.c
+++ client/renderers/EGL/egldebug.c
@@ -18,8 +18,8 @@ Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
 #include "egldebug.h"
-#include <SDL2/SDL_egl.h>
 #include <GL/gl.h>
+#include <EGL/egl.h>
 
 const char * egl_getErrorStr(void)
 {
@@ -28,18 +28,18 @@ const char * egl_getErrorStr(void)
     case EGL_SUCCESS            : return "EGL_SUCCESS";
     case EGL_NOT_INITIALIZED    : return "EGL_NOT_INITIALIZED";
     case EGL_BAD_ACCESS         : return "EGL_BAD_ACCESS";
-		case EGL_BAD_ALLOC          : return "EGL_BAD_ALLOC";
-		case EGL_BAD_ATTRIBUTE      : return "EGL_BAD_ATTRIBUTE";
-		case EGL_BAD_CONTEXT        : return "EGL_BAD_CONTEXT";
-		case EGL_BAD_CONFIG         : return "EGL_BAD_CONFIG";
-		case EGL_BAD_CURRENT_SURFACE: return "EGL_BAD_CURRENT_SURFACE";
-		case EGL_BAD_DISPLAY        : return "EGL_BAD_DISPLAY";
-		case EGL_BAD_SURFACE        : return "EGL_BAD_SURFACE";
-		case EGL_BAD_MATCH          : return "EGL_BAD_MATCH";
-		case EGL_BAD_PARAMETER      : return "EGL_BAD_PARAMETER";
-		case EGL_BAD_NATIVE_PIXMAP  : return "EGL_BAD_NATIVE_PIXMAP";
-		case EGL_BAD_NATIVE_WINDOW  : return "EGL_BAD_NATIVE_WINDOW";
-		case EGL_CONTEXT_LOST       : return "EGL_CONTEXT_LOST";
-		default                     : return "UNKNOWN";
-	}
+    case EGL_BAD_ALLOC          : return "EGL_BAD_ALLOC";
+    case EGL_BAD_ATTRIBUTE      : return "EGL_BAD_ATTRIBUTE";
+    case EGL_BAD_CONTEXT        : return "EGL_BAD_CONTEXT";
+    case EGL_BAD_CONFIG         : return "EGL_BAD_CONFIG";
+    case EGL_BAD_CURRENT_SURFACE: return "EGL_BAD_CURRENT_SURFACE";
+    case EGL_BAD_DISPLAY        : return "EGL_BAD_DISPLAY";
+    case EGL_BAD_SURFACE        : return "EGL_BAD_SURFACE";
+    case EGL_BAD_MATCH          : return "EGL_BAD_MATCH";
+    case EGL_BAD_PARAMETER      : return "EGL_BAD_PARAMETER";
+    case EGL_BAD_NATIVE_PIXMAP  : return "EGL_BAD_NATIVE_PIXMAP";
+    case EGL_BAD_NATIVE_WINDOW  : return "EGL_BAD_NATIVE_WINDOW";
+    case EGL_CONTEXT_LOST       : return "EGL_CONTEXT_LOST";
+    default                     : return "UNKNOWN";
+  }
 }
--- client/renderers/EGL/shader.c
+++ client/renderers/EGL/shader.c
@@ -25,8 +25,6 @@ Place, Suite 330, Boston, MA 02111-1307 USA
 #include <string.h>
 #include <stdio.h>
 
-#include <SDL2/SDL_egl.h>
-
 struct EGL_Shader
 {
   bool   hasShader;
