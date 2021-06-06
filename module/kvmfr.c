e4bb4bd198f60807b7a14494c9e92efb33491a46
Quantum <quantum2048@gmail.com>
2021-06-05 21:26:18 -0400

    [all] normalize copyright on all source files

--- module/kvmfr.c
+++ module/kvmfr.c
@@ -1,22 +1,22 @@
-/*
-KVMFR IVSHMEM DMA Buffer Driver
-Copyright (C) 2017-2020 Geoffrey McRae <geoff@hostfission.com>
-Copyright (C) 2021 Guanzhong Chen <quantum2048@gmail.com>
-https://looking-glass.hostfission.com
-
-This program is free software; you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free Software
-Foundation; either version 2 of the License, or (at your option) any later
-version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE. See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with
-this program; if not, write to the Free Software Foundation, Inc., 59 Temple
-Place, Suite 330, Boston, MA 02111-1307 USA
-*/
+/**
+ * Looking Glass
+ * Copyright (C) 2017-2021 The Looking Glass Authors
+ * https://looking-glass.io
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 59
+ * Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 
 #define PCI_KVMFR_VENDOR_ID 0x1af4 //Red Hat Inc,
 #define PCI_KVMFR_DEVICE_ID 0x1110 //Inter-VM shared memory
