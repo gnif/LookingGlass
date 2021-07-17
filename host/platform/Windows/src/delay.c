d1ec19b30b5767d7a9a1a51106f1b5d4ce5eba47
Geoffrey McRae <geoff@hostfission.com>
2021-07-17 15:02:58 +1000

    [host] windows: fix delayExecution order of magnitude bug

--- host/platform/Windows/src/delay.c
+++ host/platform/Windows/src/delay.c
@@ -44,6 +44,6 @@ void delayInit(void)
 
 void delayExecution(float ms)
 {
-  LARGE_INTEGER interval = { .QuadPart = -1 * (int)(ms * 1000.0f) };
+  LARGE_INTEGER interval = { .QuadPart = -1 * (int)(ms * 10000.0f) };
   NtDelayExecution(FALSE, &interval);
 }
