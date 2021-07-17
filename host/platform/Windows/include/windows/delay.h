7decb58bf7f7f2c70d5bd1ecfb17f51eca5009a2
Geoffrey McRae <geoff@hostfission.com>
2021-07-17 15:05:32 +1000

    [host] windows: fix build on Linux due to case sensitive filenames

--- host/platform/Windows/include/windows/delay.h
+++ host/platform/Windows/include/windows/delay.h
@@ -18,7 +18,7 @@
  * Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
-#include <Windows.h>
+#include <windows.h>
 
 typedef NTSTATUS (__stdcall *ZwSetTimerResolution_t)(ULONG RequestedResolution,
     BOOLEAN Set, PULONG ActualResolution);
