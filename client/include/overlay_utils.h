edabd1bae79ed95fae54ec8d14d09433b56d1e51
Quantum <quantum2048@gmail.com>
2021-07-29 21:55:14 -0400

    [client] imgui: remove no longer used overlay_utils.cpp

--- client/include/overlay_utils.h
+++ client/include/overlay_utils.h
@@ -23,18 +23,9 @@
 
 #include "common/types.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 typedef struct ImVec2 ImVec2;
 
 void overlayGetImGuiRect(struct Rect * rect);
 ImVec2 * overlayGetScreenSize(void);
-void imGuiResetStyle(void);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
