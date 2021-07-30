67c4026e35b6b53e046dc872476253fbc1eab99a
Quantum <quantum2048@gmail.com>
2021-07-30 07:15:41 -0400

    [client] main: prevent the user from launching looking glass as setuid
    We don't want to encourage craziness of people making the client suid to
    bypass permission issues on the shm file.
    
    Note: I see no evidence of this happening in the wild, but let's be
    proactive.

--- client/src/main.c
+++ client/src/main.c
@@ -1189,6 +1189,12 @@ int main(int argc, char * argv[])
     return -1;
   }
 
+  if (getuid() != geteuid())
+  {
+    DEBUG_ERROR("Do not run looking glass as setuid!");
+    return -1;
+  }
+
   DEBUG_INFO("Looking Glass (%s)", BUILD_VERSION);
   DEBUG_INFO("Locking Method: " LG_LOCK_MODE);
 
