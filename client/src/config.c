4b8644bdf32e730962469ebc3fe8db6e57f288c7
Quantum <quantum2048@gmail.com>
2021-08-04 05:48:32 -0400

    [all] copyright: use unicode copyright sign ©
    This is done for consistency with the license strings in appstrings.c.

--- client/src/config.c
+++ client/src/config.c
@@ -1,6 +1,6 @@
 /**
  * Looking Glass
- * Copyright (C) 2017-2021 The Looking Glass Authors
+ * Copyright © 2017-2021 The Looking Glass Authors
  * https://looking-glass.io
  *
  * This program is free software; you can redistribute it and/or modify it
@@ -651,7 +651,7 @@ static void doLicense(void)
     // BEGIN LICENSE BLOCK
     "\n"
     "Looking Glass\n"
-    "Copyright (C) 2017-2021 The Looking Glass Authors\n"
+    "Copyright © 2017-2021 The Looking Glass Authors\n"
     "https://looking-glass.io\n"
     "\n"
     "This program is free software; you can redistribute it and/or modify it under\n"
--- client/src/overlay_utils.c
+++ client/src/overlay_utils.c
@@ -1,6 +1,6 @@
 /**
  * Looking Glass
- * Copyright (C) 2017-2021 The Looking Glass Authors
+ * Copyright © 2017-2021 The Looking Glass Authors
  * https://looking-glass.io
  *
  * This program is free software; you can redistribute it and/or modify it
