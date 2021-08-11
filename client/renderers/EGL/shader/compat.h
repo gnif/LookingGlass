02d75704501a0fabd63349372d64ca66b66ba078
Quantum <quantum2048@gmail.com>
2021-08-10 21:41:48 -0400

    [client] egl: remove textureGather implementation in compat.h
    The shader works correctly with #version 320 es and the normal textureGather,
    so the problem seems to be the version in compat.h. Since our implementation
    needs uInRes[0], we should just move the fallback into ffx_fsr1_easu.frag.

--- client/renderers/EGL/shader/compat.h
+++ client/renderers/EGL/shader/compat.h
@@ -1,20 +1,3 @@
-#if __VERSION__ == 300
-  vec4 textureGather(sampler2D tex, vec2 uv, int comp)
-  {
-    vec4 c0 = textureOffset(tex, uv, ivec2(0,1));
-    vec4 c1 = textureOffset(tex, uv, ivec2(1,1));
-    vec4 c2 = textureOffset(tex, uv, ivec2(1,0));
-    vec4 c3 = textureOffset(tex, uv, ivec2(0,0));
-    return vec4(c0[comp], c1[comp], c2[comp],c3[comp]);
-  }
-#elif __VERSION__ < 300
-  vec4 textureGather(sampler2D tex, vec2 uv, int comp)
-  {
-    vec4 c3 = texture2D(tex, uv);
-    return vec4(c3[comp], c3[comp], c3[comp],c3[comp]);
-  }
-#endif
-
 #if __VERSION__ < 310
   uint bitfieldExtract(uint val, int off, int size)
   {
