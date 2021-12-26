6ef3fea05e5d22eee43701c306ba41f9aa709f17
Jonathan Rubenstein <jrubcop@gmail.com>
2021-12-26 07:00:49 +0200

    [doc] Version no longer gets spellchecked

---
+++ doc/lgrelease.py
@@ -0,0 +1,17 @@
+# -- Path setup --------------------------------------------------------------
+from pathlib import Path
+
+release = None
+
+try:
+    with open(Path(__file__).parent.parent / 'VERSION') as f:
+        release = f.read().strip()
+except IOError:
+    import subprocess
+    try:
+        release = subprocess.check_output([
+            'git', 'describe', '--always', '--abbrev=10', '--dirty=+', '--tags'
+        ]).decode('utf-8').strip()
+    except (subprocess.CalledProcessError, OSError):
+        release = '(unknown version)'
+    del subprocess
