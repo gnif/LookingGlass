5bc99872a315fb1a5870af4d5acf0684a0a653d3
Jonathan Rubenstein <jrubcop@gmail.com>
2021-12-26 05:08:43 +0200

    [doc] conf: Fix typo in release git-describe command
    We got the gist of it, but in the end, didn't git it right.

--- doc/conf.py
+++ doc/conf.py
@@ -30,7 +30,7 @@ except IOError:
     import subprocess
     try:
         release = subprocess.check_output([
-            'gist', 'describe', '--always', '--abbrev=10', '--dirty=+', '--tags'
+            'git', 'describe', '--always', '--abbrev=10', '--dirty=+', '--tags'
         ]).decode('utf-8').strip()
     except (subprocess.CalledProcessError, OSError):
         release = '(unknown version)'
