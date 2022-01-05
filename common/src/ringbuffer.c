952ebea2c5c349a45bb6b5c4c6b44cd68dbf67ca
Geoffrey McRae <geoff@hostfission.com>
2022-01-05 19:42:46 +1100

    [all] refresh copyright dates

--- common/src/ringbuffer.c
+++ common/src/ringbuffer.c
@@ -1,6 +1,6 @@
 /**
  * Looking Glass
- * Copyright © 2017-2021 The Looking Glass Authors
+ * Copyright © 2017-2022 The Looking Glass Authors
  * https://looking-glass.io
  *
  * This program is free software; you can redistribute it and/or modify it
