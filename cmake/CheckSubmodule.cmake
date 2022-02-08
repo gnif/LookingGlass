e1ac838796b754296f700751567106487f5a3c8a
Tudor Brindus <me@tbrindus.ca>
2022-02-07 23:52:21 -0500

    [client] wayland: use `wayland-protocols` from submodule

--- cmake/CheckSubmodule.cmake
+++ cmake/CheckSubmodule.cmake
@@ -2,7 +2,8 @@ if (EXISTS "${PROJECT_TOP}/.git" AND (
     (NOT EXISTS "${PROJECT_TOP}/repos/cimgui/.git") OR
     (NOT EXISTS "${PROJECT_TOP}/repos/LGMP/.git") OR
     (NOT EXISTS "${PROJECT_TOP}/repos/PureSpice/.git") OR
-    (NOT EXISTS "${PROJECT_TOP}/repos/cimgui/imgui/.git")
+    (NOT EXISTS "${PROJECT_TOP}/repos/cimgui/imgui/.git") OR
+    (NOT EXISTS "${PROJECT_TOP}/repos/wayland-protocols/.git")
 ))
     message(FATAL_ERROR "Submodules are not initialized. Run\n\tgit submodule update --init --recursive")
 endif()
