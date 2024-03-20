1afeb9e4ac8bb6c5fb4e8dcfdebd11cd5c01d386
kamplom <476.1492.1789m@gmail.com>
2024-03-20 21:03:20 +0100

    Use full buffer for viewport source

--- client/displayservers/Wayland/gl.c
+++ client/displayservers/Wayland/gl.c
@@ -102,7 +102,7 @@ void waylandEGLSwapBuffers(EGLDisplay display, EGLSurface surface, const struct 
       wl_surface_set_buffer_scale(wlWm.surface, 1);
       if (!wlWm.viewport)
         wlWm.viewport = wp_viewporter_get_viewport(wlWm.viewporter, wlWm.surface);
-      wp_viewport_set_source(wlWm.viewport, 0, 0, width * wlWm.scale, height * wlWm.scale);
+      wp_viewport_set_source(wlWm.viewport, wl_fixed_from_int(-1), wl_fixed_from_int(-1) , wl_fixed_from_int(-1), wl_fixed_from_int(-1));
       wp_viewport_set_destination(wlWm.viewport, width, height);
     }
     else
