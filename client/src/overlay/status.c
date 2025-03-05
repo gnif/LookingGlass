b43f572af0c7186597360a74ea82a99972d8ca78
Geoffrey McRae <geoff@hostfission.com>
2025-03-05 15:17:44 +1100

    Revert "[client] overlay/status: don't return damage rect if nothing was drawn"
    This reverts commit 7e9e38faa5d3144dac9e9c3abd357435b25c16ff as it
    causes screen corruption when moving imgui overlay dialogs around.

--- client/src/overlay/status.c
+++ client/src/overlay/status.c
@@ -89,7 +89,6 @@ static int status_render(void * udata, bool interactive, struct Rect * windowRec
   };
 
   int xPos = screen->x - marginX;
-  bool show = false;
   for(int i = 0; i < LG_USER_STATUS_MAX; ++i)
   {
     OverlayImage * img = &l_image[i];
@@ -99,8 +98,6 @@ static int status_render(void * udata, bool interactive, struct Rect * windowRec
     // if the recording indicator is off, don't draw but reserve space
     if (i == LG_USER_STATUS_RECORDING && !l_recordToggle)
       goto next;
-
-    show = true;
 
     ImDrawList_AddImage(
       igGetBackgroundDrawList_Nil(),
@@ -120,9 +117,6 @@ static int status_render(void * udata, bool interactive, struct Rect * windowRec
 next:
     xPos -= ICON_SIZE + gapX;
   }
-
-  if (!show)
-    return 0;
 
   *windowRects = rect;
   return 1;
