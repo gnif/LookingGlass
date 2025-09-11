871261c11290610c650987816680bb3097655da4
Quantum <quantum2048@gmail.com>
2025-09-11 01:25:47 -0400

    [idd] all: use UTF-8 source code

--- idd/LGCommon/PipeMsg.h
+++ idd/LGCommon/PipeMsg.h
@@ -1,6 +1,6 @@
 /**
  * Looking Glass
- * Copyright © 2017-2025 The Looking Glass Authors
+ * Copyright Â© 2017-2025 The Looking Glass Authors
  * https://looking-glass.io
  *
  * This program is free software; you can redistribute it and/or modify it
