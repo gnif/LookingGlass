94550e09b4c3f664611f8d044ce2a766b495cbf5
Quantum <quantum2048@gmail.com>
2025-09-13 15:04:03 -0400

    [idd] helper: use CWindow helper to avoid global state

---
+++ idd/LGIddHelper/CWindow.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <shellapi.h>
+
+#define WM_NOTIFY_ICON (WM_USER)
+
+class CWindow {
+  static ATOM s_atom;
+  static LRESULT CALLBACK wndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+  HWND m_hwnd = NULL;
+  LRESULT handleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+public:
+  static bool registerClass();
+  CWindow();
+  ~CWindow();
+};
