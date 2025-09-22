7636b60e531b308657319ab257ad81554b088d6b
Quantum <quantum2048@gmail.com>
2025-09-22 01:54:33 -0400

    [idd] helper/resize: move logic to .cpp and add error checking

---
+++ idd/LGIddHelper/UIHelpers.cpp
@@ -0,0 +1,17 @@
+﻿#include "UIHelpers.h"
+#include <CDebug.h>
+
+WidgetPositioner::~WidgetPositioner()
+{
+  if (!EndDeferWindowPos(hdwp))
+    DEBUG_ERROR_HR(GetLastError(), "EndDeferWindowPos");
+}
+
+void WidgetPositioner::move(HWND child, int x, int y, int cx, int cy)
+{
+  HDWP next = DeferWindowPos(hdwp, child, nullptr, x, y, cx, cy, SWP_NOACTIVATE | SWP_NOZORDER);
+  if (next)
+    hdwp = next;
+  else
+    DEBUG_ERROR_HR(GetLastError(), "DeferWindowPos");
+}
