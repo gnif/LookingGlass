e462430b63d487461d49df8f1206751d5c9d391a
Quantum <quantum2048@gmail.com>
2025-09-23 02:20:00 -0400

    [idd] helper: use common control v6 with styles

--- idd/LGIddHelper/CWidget.cpp
+++ idd/LGIddHelper/CWidget.cpp
@@ -1,5 +1,9 @@
 ﻿#include "CWidget.h"
 
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
 HWND CWidget::createWindowSimple(LPCWSTR cls, LPCWSTR title, DWORD style, HWND parent, DWORD dwExStyle)
 {
   return CreateWindowEx(dwExStyle, cls, title, style, 0, 0, 0, 0, parent,
