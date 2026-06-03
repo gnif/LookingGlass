29d02ddbb90deb949ddda5adf50087626cc7e14c
Quantum <quantum2048@gmail.com>
2026-06-03 01:19:34 -0400

    [idd] helper: add checkbox widget

---
+++ idd/LGIddHelper/CCheckbox.cpp
@@ -0,0 +1,38 @@
+﻿/**
+ * Looking Glass
+ * Copyright © 2017-2026 The Looking Glass Authors
+ * https://looking-glass.io
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 59
+ * Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#include "CCheckbox.h"
+#include <windowsx.h>
+#include <CDebug.h>
+
+CCheckbox::CCheckbox(LPCWSTR title, DWORD style, HWND parent) :
+  CButton(title, style | BS_CHECKBOX, parent)
+{
+}
+
+bool CCheckbox::isChecked()
+{
+  return Button_GetCheck(m_hwnd) == BST_CHECKED;
+}
+
+void CCheckbox::setChecked(bool checked)
+{
+  Button_SetCheck(m_hwnd, checked ? BST_CHECKED : BST_UNCHECKED);
+}
