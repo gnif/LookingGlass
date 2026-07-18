fcd66a51720e461e783ecd3d95d76650ae8b8056
Geoffrey McRae <geoff@hostfission.com>
2026-07-19 03:31:44 +1000

    [all] fix copyright headers

--- client/renderers/EGL/filter_glsl.c
+++ client/renderers/EGL/filter_glsl.c
@@ -1,6 +1,6 @@
 /**
  * Looking Glass
- * Copyright Â© 2017-2026 The Looking Glass Authors
+ * Copyright © 2017-2026 The Looking Glass Authors
  * https://looking-glass.io
  *
  * This program is free software; you can redistribute it and/or modify it
@@ -12,6 +12,10 @@
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
  * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 59
+ * Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
 #define _GNU_SOURCE
