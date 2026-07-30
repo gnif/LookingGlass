8a316777c86372221d45df4bfea007dd59ddd6e1
Geoffrey McRae <geoff@hostfission.com>
2026-07-30 12:05:00 +1000

    [client] overlay: add cyrillic glyphs to the imgui atlas

--- client/src/main.c
+++ client/src/main.c
@@ -1112,6 +1112,9 @@ static int lg_run(void)
   ImVector_ImWchar_Init(&g_state.fontRange);
   ImFontGlyphRangesBuilder * rangeBuilder =
     ImFontGlyphRangesBuilder_ImFontGlyphRangesBuilder();
+  ImFontGlyphRangesBuilder_AddRanges(
+    rangeBuilder,
+    ImFontAtlas_GetGlyphRangesCyrillic(g_state.io->Fonts));
   ImFontGlyphRangesBuilder_AddRanges(rangeBuilder, (ImWchar[]) {
     0x0020, 0x00FF, // Basic Latin + Latin Supplement
     0x2190, 0x2193, // four directional arrows
